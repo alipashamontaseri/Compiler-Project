8c123f6f51551f2e9978d410af8830f409c95a59
Allliance <alians310322@gmail.com>
2023-12-13 18:55:19 +0330

    Clean redundant files

--- src/test.py
+++ src/test.py
@@ -8,4 +8,4 @@ sys.path.append(parent)
 if __name__ == "__main__":
     phase = 2
     tester = Tester(os.path.join('..', "test_data", "P" + str(phase)))
-    tester.test(phase, keep_output=True, passive=True)
+    tester.test(phase, keep_output=False, passive=True)
