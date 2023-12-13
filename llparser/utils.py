641878b282b9c8a40aa7a18d35575bf06b11c2e8
Allliance <alians310322@gmail.com>
2023-12-13 10:55:59 +0330

    Update grammar and add grammar formatter

--- llparser/utils.py
+++ llparser/utils.py
@@ -19,6 +19,33 @@ def read_set_from_file(input_file):
         
     return result
 
+def format_raw_grammar(input_file, output_file):
+    with open(input_file, "r") as f:
+        formatted_lines = []
+        lines = f.readlines()
+        
+        for line in lines:
+            # Drop the first part, the number of line
+            parts = line.split()[1:]
+            
+            lhs = parts[0]
+            current_rule = [lhs]
+            # Excluding ->
+            for part in parts[2:]:
+                if part == "|":
+                    formatted_lines.append(" ".join(current_rule.copy()))
+                    current_rule = [lhs]
+                elif part == "EPSILON":
+                    # Special case for epsilon
+                    formatted_lines.append(f"{lhs} ")
+                    current_rule = [lhs]
+                else:
+                    current_rule.append(part)
+            if len(current_rule) > 1:
+                formatted_lines.append(" ".join(current_rule.copy()))
+        with open(output_file, "w") as o:
+            o.write("\n".join(formatted_lines))
+
 def save_dict(dictionary, filepath):
     with open(filepath, 'w') as f:
         f.write(json.dumps(dictionary))
@@ -41,7 +68,7 @@ def read_rules(filepath):
 
     return result
 
-
+format_raw_grammar(os.path.join("grammar", "raw_grammar.txt"), os.path.join("grammar", "Grammar.txt"))
 # save_dict(read_set_from_file(os.path.join("grammar", "First-Sets.txt")), os.path.join("grammar", "first.set"))
 # save_dict(read_set_from_file(os.path.join("grammar", "Follow-Sets.txt")), os.path.join("grammar", "follow.set"))
-save_dict(read_rules(os.path.join("grammar", "Grammar.txt")), os.path.join("grammar", "rules"))
+# save_dict(read_rules(os.path.join("grammar", "Grammar.txt")), os.path.join("grammar", "rules"))
