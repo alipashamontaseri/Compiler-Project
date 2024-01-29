cb70881522b2cb1bb7a72cc32312d42c0ad4a167
Kalam132 <alipasha.montaseri@gmail.com>
2024-01-29 16:44:10 +0330

    defining variables implemented

--- src/codegen/parser.py
+++ src/codegen/parser.py
@@ -34,15 +34,26 @@ class Parser:
 
 
         parse_table = defaultdict(lambda: defaultdict(lambda: []))
-        self.code_gen_list = []
+        self.code_gen_list = [] # should contain 4 address codes
 
         self.syntax_error = []
         self.semantic_error = []
 
         self.semantic_stack = []
+        self.symbol_table_heap = defaultdict(lambda: []) # dict -> list[(address, type, size)]
+        self.symbol_table_stack = defaultdict(lambda: []) # dict -> list[(address, type, size, scope)]
+        self.scope_stack = []
+
         self.stack_pointer_addr = 1
         self.base_pointer_addr = 2
         self.stack_pointer_start = 1000
+        self.base_pointer_diff = 0
+        self.heap_pointer_addr = 10
+
+        # initializing BP and SP
+        self.code_gen_list.append([0, 'ASSIGN', f'#{self.stack_pointer_start}', f'{self.stack_pointer_addr}', ''])
+        self.code_gen_list.append([1, 'ASSIGN', f'#{self.stack_pointer_start}', f'{self.base_pointer_addr}', ''])
+
 
 
         self.has_epsilon = defaultdict(lambda: False)
@@ -78,10 +89,50 @@ class Parser:
 
         self.parse_table = parse_table
     
-    def handle_action(self, action):
-        if action == 'pnext':
+    def add_to_symbol_heap(self, varname, vartype, varsize):
+        self.symbol_table_heap[varname].append([self.heap_pointer_addr, vartype, varsize])
+        self.heap_pointer_addr += int(varsize)
 
-        
+    def add_to_symbol_stack(self, varname, vartype, varsize):
+        self.symbol_table_stack[varname].append([self.base_pointer_diff, vartype, varsize, len(self.scope_stack)])
+        self.base_pointer_diff += int(varsize)
+
+    def pnext_action(self):
+        # print("SANI MADAR JENDE",self.look_ahead.lexeme, self.look_ahead.line_number, self.look_ahead.token_class)
+        self.semantic_stack.append(self.look_ahead)
+
+    def type_action(self):
+        self.semantic_stack.append('$')
+        self.semantic_stack.append(self.look_ahead)
+
+    def pvar_action(self):
+        until_dollar = [] 
+        while(self.semantic_stack[-1] != '$'):
+            until_dollar.append(self.semantic_stack.pop())
+        print(until_dollar)
+        self.semantic_stack.pop()
+        if len(until_dollar) == 2: # simple int
+            if len(self.scope_stack) == 0: # global variable
+                self.add_to_symbol_heap(until_dollar[0].lexeme, 'int', 1)
+            else: # local variable
+                self.add_to_symbol_stack(until_dollar[0].lexeme, 'int', 1)
+        elif len(until_dollar) == 3: # array
+            if len(self.scope_stack) == 0: # global variable
+                self.add_to_symbol_heap(until_dollar[1].lexeme, 'array', until_dollar[0].lexeme)
+            else: # local variable
+                self.add_to_symbol_stack(until_dollar[1].lexeme, 'array', until_dollar[0].lexeme)
+        else:
+            raise Exception("error while defining a variable in semantic stack")
+
+    def handle_actions(self, action):
+        print(action)
+        if action == 'pnext':
+            self.pnext_action()
+        if action == 'type':
+            self.type_action()
+        if action == 'pvar':
+           self.pvar_action()
+           print(self.symbol_table_heap)
 
     def get_next_token(self):
         if self.look_ahead and self.look_ahead.get_terminal() == END_TOKEN:
@@ -120,7 +171,7 @@ class Parser:
                 current_path.pop(-1)
                 continue
             if not stack[-1][1] and stack[-1][0][0] == '#':
-                self.handle_action(stack[-1][0][1:])
+                self.handle_actions(stack[-1][0][1:])
                 stack.pop(-1)
                 continue 
 
@@ -193,7 +244,7 @@ class Parser:
 
     def write_code_gen(self):
         with open(self.code_gen_file, 'w', encoding="utf-8") as f:
-            f.write('\n'.join([f'{line_no}\t{content}' for line_no, content in self.code_gen_list]))
+            f.write('\n'.join([f'{line_no}\t({x}, {y}, {z}, {t})' for line_no,x,y,z,t in self.code_gen_list]))
 
     def write_semantic_error(self):
         pass
